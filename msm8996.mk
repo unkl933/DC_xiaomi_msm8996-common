4f4b0f7b1119987b69fce9e5994a371247d2bf02
dopamine <crack1933@gmail.com>
2022-02-21 08:08:30 +0700

    Speed profile services and wifi-service to reduce RAM

--- msm8996.mk
+++ msm8996.mk
@@ -481,6 +481,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Tethering
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay
