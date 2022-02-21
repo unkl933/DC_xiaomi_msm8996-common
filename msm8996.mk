71cd0a27257a2fa625da96b475151d08a0943937
dopamine <crack1933@gmail.com>
2022-02-21 08:03:13 +0700

    Conditionally build IR HIDL

--- msm8996.mk
+++ msm8996.mk
@@ -187,6 +187,15 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# ConsumerIR
+ifeq ($(BOARD_HAVE_IR),true)
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-service.xiaomi_sdm660
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+endif
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
