96bad86ed6ef85a587e138bd69b02adab54193ee
dopamine <crack1933@gmail.com>
2022-03-04 03:59:19 +0700

    Revert "Add ButtonSettings in DeviceFeatures"
    This reverts commit b7d4ee90ecfd43892e7ee27fb55c71a6f7457dd6.

--- msm8996.mk
+++ msm8996.mk
@@ -142,10 +142,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
-# Button Settings
-PRODUCT_PACKAGES += \
-    ButtonSettings
-
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.3:64 \
