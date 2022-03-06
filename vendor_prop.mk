08cb7ffac767653b02eb0acd906d39e6643e090a
dopamine <crack1933@gmail.com>
2022-03-06 22:25:41 +0700

    drop buffer triple

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -168,9 +168,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_rotator_split=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
-    vendor.gralloc.enable_fb_ubwc=1 \
-    ro.surface_flinger.set_idle_timer_ms=9000 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+    vendor.gralloc.enable_fb_ubwc=1
 
 # Surfaceflinger
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -178,9 +176,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
-
+    ro.surface_flinger.set_idle_timer_ms=9000 \
+    ro.surface_flinger.max_virtual_display_dimension=4096
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
