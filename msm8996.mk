4fb88687af9a5403d82d09efb4e8bd87eb2fc730
dopamine <crack1933@gmail.com>
2022-03-07 13:48:25 +0700

    build audio.primary.default

--- msm8996.mk
+++ msm8996.mk
@@ -102,7 +102,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.soundtrigger@2.2-impl:32 \
     audio.a2dp.default \
-    audio.primary.msm8996 \
+    audio.primary.default \
     audio.r_submix.default \
     audio.usb.default \
     libaacwrapper \
