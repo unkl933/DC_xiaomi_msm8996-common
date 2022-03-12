20dcd6d3eace75fbafba8fb4c95976c9d8de2c19
dopamine <crack1933@gmail.com>
2022-03-13 01:05:37 +0700

    build android.hardware.drm@1.4.vendor

--- msm8996.mk
+++ msm8996.mk
@@ -224,7 +224,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # For config.fs
 PRODUCT_PACKAGES += \
