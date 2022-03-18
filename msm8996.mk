a9abe2b1b20043761ab0197abc0fb215f4508a20
dopamine <crack1933@gmail.com>
2022-03-19 00:56:56 +0700

    Revert "build android.hardware.drm@1.4.vendor"
    This reverts commit 20dcd6d3eace75fbafba8fb4c95976c9d8de2c19.

--- msm8996.mk
+++ msm8996.mk
@@ -223,8 +223,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.4.vendor
+    android.hardware.drm@1.4-service.clearkey
 
 # For config.fs
 PRODUCT_PACKAGES += \
