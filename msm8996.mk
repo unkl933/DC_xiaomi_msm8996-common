6cf443de19f1b3be824e63e8a491758caec5ec55
dopamine <crack1933@gmail.com>
2022-03-19 22:38:38 +0700

    extphonelib

--- msm8996.mk
+++ msm8996.mk
@@ -462,6 +462,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
     ims-ext-common \
     ims_ext_common.xml \
     qti-telephony-hidl-wrapper \
