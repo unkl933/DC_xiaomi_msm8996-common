c5f9dcde9a342cef38bff9da442e63da06cad8a0
dopamine <crack1933@gmail.com>
2022-03-20 22:38:47 +0700

    split sepolicy

--- msm8996.mk
+++ msm8996.mk
@@ -312,6 +312,10 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl:64 \
     android.hardware.keymaster@3.0-service
 
+# Keystore
+PRODUCT_PACKAGES += \
+    android.system.keystore2
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_8996
