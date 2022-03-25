d8d632436a39cd7698dd35d135c9704be71966c8
dopamine <crack1933@gmail.com>
2022-03-25 23:50:33 +0700

    Revert "Nuke Power-libperfmgr"
    This reverts commit 59e197694db004f43894c90a86b79dbb002872f7.

--- msm8996.mk
+++ msm8996.mk
@@ -374,10 +374,10 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_msm8996
+    android.hardware.power-service.xiaomi-msm8996-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/config/msm8996/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # Touch HAL
 PRODUCT_PACKAGES += \
