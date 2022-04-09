df83a9fb91b652f553ad043b56ce16b1065857db
dopamine <crack1933@gmail.com>
2022-04-09 23:08:35 +0700

    Update msm8996.mk

--- msm8996.mk
+++ msm8996.mk
@@ -523,3 +523,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# DeviceSettings
+PRODUCT_PACKAGES += \
+   DeviceSettings
